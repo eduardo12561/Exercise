1f0136740f9b69e8ffd71306f2e093e34472f9f2
Eduardo Breda <edu12561@hotmail.com>
2021-05-24 14:19:13 -0300

    second commit

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ const formatDate = (date = new Date()) => {
 
 console.log(formatDate());
 
-/* Exercise 6
+/* Exercise 5
 -----------------
 Write a JavaScript program to create a new string adding "New!" in front of a given string. if the given string begins with "New!" already then return the original string
 */
